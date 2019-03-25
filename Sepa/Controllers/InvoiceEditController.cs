c8d96c568fd7598e1d8b171c9a9562f321187f61
Colm Carberry <ccarberry@live.com>
2019-03-25 21:57:47 +0000

    Update Invoice Scripts and Test XML Code

---
+++ Sepa/Controllers/InvoiceEditController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+using System.IO;
+using Sepa.DAL;
+using Sepa.Models;
+
+namespace Sepa.Controllers
+{
+    public class InvoiceEditController : Controller
+    {
+        private SepaContext db = new SepaContext();
+        // GET: InvoiceUpdate
+        public ActionResult Index()
+        {
+        
+        var data = from item in db.Invoices
+
+        where item.Invoice_ID < 10953
+        orderby item.Invoice_ID
+
+        select item;
+
+        return View(data.ToList());
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(List<Invoice> invoices)
+
+        {
+     
+            foreach (Invoice inv in invoices)
+
+            {
+
+                Invoice Existed_Inv = db.Invoices.Find(inv.Invoice_ID);
+
+                Existed_Inv.Posting_Date = inv.Posting_Date;
+
+                Existed_Inv.Posting_Desc = inv.Posting_Desc;
+
+                Existed_Inv.StatusCode = inv.StatusCode;
+
+            }
+
+            db.SaveChanges();
+            //return RedirectToAction("Index");
+            return View();
+
+        }
+    }
+}
+
