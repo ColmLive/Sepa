53086e669feb88d6ebbe56fd32d83aad3156bf4c
Colm Carberry <ccarberry@live.com>
2019-03-27 09:27:34 +0000

    Modify Menu Sytem to include basic views for Vendor & Invoice

--- Sepa/Controllers/HomeController.cs
+++ Sepa/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Sepa.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "SEPA Pilot Application";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "SEPA Pilot";
 
             return View();
         }
