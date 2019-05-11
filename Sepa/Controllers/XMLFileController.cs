4b92387acb226a8fd708d19dace8ec646be95ae1
Colm Carberry <ccarberry@live.com>
2019-05-11 16:58:37 +0100

    XMLPaymentFileCreationbyVendor

--- Sepa/Controllers/XMLFileController.cs
+++ Sepa/Controllers/XMLFileController.cs
@@ -19,12 +19,12 @@ namespace Sepa.Controllers
 
                       where item.StatusCode == Status.SEPA
                       orderby item.Vendor_ID, item.Invoice_ID
-                       select item;
+                      select item;
 
             using (XmlWriter writer = XmlWriter.Create(@"c:\temp\test.xml"))
             {
                 writer.WriteStartDocument();
-               
+
                 writer.WriteStartElement("Invoices");
 
                 foreach (Invoice invoice in inv)
@@ -44,39 +44,106 @@ namespace Sepa.Controllers
                 writer.WriteEndElement();
                 writer.WriteEndDocument();
             }
-            var data = from b in db.Invoices
-                       where b.StatusCode == Status.SEPA
-                       group b by b.StatusCode   into g
-                       select new Group<string, Invoice> { Key = g.Key.ToString(), Values = g };
+
+            //*** Generate Payment File to post to SEPA System ***//
+
+            //*** Part 1 - Generate Summary Totals for XML File ***//
+            var query = db.Invoices.GroupBy(g => new
+            {
+                g.StatusCode
+            })
+             .Select(group => new
+             {
+                 StatusCode = group.Key.StatusCode,
+                 TotalAmount = group.Sum(a => a.Invoice_Value),
+                 TotalCount = group.Count()
+             });
+
+            //*** Write SEPA Totals to XML File ***//
+
             using (XmlWriter writer = XmlWriter.Create(@"c:\temp\pay.xml"))
             {
                 writer.WriteStartDocument();
                 writer.WriteStartElement("Payments");
                 writer.WriteStartElement("Summary");
-                writer.WriteElementString("Totals", .ToString());
 
-                writer.WriteEndElement();
-                
+                //*** Select only SEPA Status ***//
 
-                foreach (Invoice invoice in inv)
+                foreach (var item in query.Where(a => a.StatusCode == Status.SEPA))
                 {
-                    writer.WriteStartElement("Invoice");
+                    writer.WriteElementString("Totals", item.TotalAmount.ToString());
+                    writer.WriteElementString("Totals", item.TotalCount.ToString());
+                    writer.WriteElementString("Totals", item.StatusCode.ToString());
 
-                    writer.WriteElementString("InvoiceID", invoice.Invoice_ID.ToString());
-                    writer.WriteElementString("InvoiceValue", invoice.Invoice_Value.ToString());
-                    writer.WriteElementString("PostingDate", invoice.Posting_Date.ToString());
-                    writer.WriteElementString("PostingDesc", invoice.Posting_Desc.ToString());
-                    writer.WriteElementString("VendorID", invoice.Vendor_ID.ToString());
-                    writer.WriteElementString("VendorInvNo", invoice.Vendor_InvNo.ToString());
-                    writer.WriteElementString("Currency", invoice.Currency_Code);
-                    writer.WriteEndElement();
                 }
-
                 writer.WriteEndElement();
-                writer.WriteEndDocument();
-            }
 
-            return View();
+                //*** Part 2 - Generate Summary Totals for Payment Information Block ***//
+                var query2 = db.Invoices.GroupBy(g => new
+                {
+                    g.StatusCode
+                })
+                 .Select(group => new
+                 {
+                     StatusCode = group.Key.StatusCode,
+                     TotalAmount = group.Sum(a => a.Invoice_Value),
+                     TotalCount = group.Count()
+                 });
+
+                //*** Write Payment Information Totals to XML File ***//
+                {
+                    //writer.WriteStartDocument();
+                    writer.WriteStartElement("PIB");
+                    writer.WriteStartElement("Summary");
+
+                    //*** Select only SEPA Status ***//
+
+                    foreach (var item in query2.Where(a => a.StatusCode == Status.SEPA))
+                    {
+                        writer.WriteElementString("PIBTotals", item.TotalAmount.ToString());
+                        writer.WriteElementString("PIBTotals", item.TotalCount.ToString());
+                        writer.WriteElementString("PIBTotals", item.StatusCode.ToString());
+
+                    }
+                    writer.WriteEndElement();
+
+                    //*** Part 3 - Generate a single payment foe each Payee ***//
+                    var query3 = db.Invoices.GroupBy(g => new
+                    {
+                        g.Vendors.Vendor_Name
+                    })
+                     .Select(group => new
+                     {
+                         StatusCode = group.Key.Vendor_Name,
+                         TotalAmount = group.Sum(a => a.Invoice_Value),
+                         TotalCount = group.Count()
+                     });
+
+                    //*** Write Payment Information Totals to XML File ***//
+
+                    {
+                        //writer.WriteStartDocument();
+                        writer.WriteStartElement("Dbtr");
+                        //writer.WriteStartElement("Summary");
+
+                        //*** Select only SEPA Status ***//
+
+                        foreach (var item in query3)
+                        {
+                            writer.WriteElementString("PIBTotals", item.TotalAmount.ToString());
+                            writer.WriteElementString("PIBTotals", item.TotalCount.ToString());
+                            writer.WriteElementString("PIBTotals", item.StatusCode.ToString());
+
+                        }
+                        writer.WriteEndElement();
+
+
+                        writer.WriteEndDocument();
+                    }
+
+                    return View();
+                }
+            }
         }
     }
 }
