6b72213fb8c40f7a9ec27ceed9899df8db1a9b32
Colm Carberry <ccarberry@live.com>
2019-05-13 20:37:13 +0100

    VendorUpdateControllerTest

--- Sepa/Controllers/VendorUpdateController.cs
+++ Sepa/Controllers/VendorUpdateController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Data.Entity;
 using System.Web.Mvc;
 using Sepa.DAL;
+using System.Data;
+using System.Data.SqlClient;
 using Sepa.Models;
 
 
@@ -20,7 +22,8 @@ namespace Sepa.Controllers
        
             using (SepaContext dBEntities = new SepaContext())
             {
-                dBEntities.Database.ExecuteSqlCommand("insert into Vendors values(998,'vendor tes2','Add6', 'New York', 'USA', 'USD/PL', 'v3@test.com', '0015552316777');");
+                SqlCommand cmd = new SqlCommand("PopulateVendors", conn)
+                //dBEntities.Database.ExecuteSqlCommand("insert into Vendors values(998,'vendor tes2','Add6', 'New York', 'USA', 'USD/PL', 'v3@test.com', '0015552316777');");
                 //                var query = dBEntities.Vendors.SqlQuery("select * from Vendors").ToList<Table1>();
                 //                var query2 = dBEntities.Database.SqlQuery<Table1>("select * from Table1").ToList<Table1>();
                 //               var query3 = dBEntities.Database.SqlQuery<tablevm>("select t1.id, t1.product, SUM(t1.price) as price from Table1 as t1 group by t1.id, t1.product").ToList<tablevm>();
