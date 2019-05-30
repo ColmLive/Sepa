0969269d45040a9c2a2a8c200093408bd3020475
Colm Carberry <ccarberry@live.com>
2019-05-30 21:55:10 +0100

    VendorUpdateControllerAnonymiseData

--- Sepa/Controllers/VendorUpdateController.cs
+++ Sepa/Controllers/VendorUpdateController.cs
@@ -20,15 +20,11 @@ namespace Sepa.Controllers
         public ActionResult Index()
         {
 
-            //using (SepaContext dBEntities = new SepaContext())
             using (var conn = new SqlConnection(db.Database.Connection.ConnectionString))
             {
                 //SqlConnection conn = new SqlConnection());
-
-                //
                 //SqlConnection("Server=(SQLEXPRESS);DataBase=SEPA;Integrated Security=SSPI");
                 //SqlConnection("Server=(localdb);DataBase=SEPA;Integrated Security=SSPI");
-
 
                 conn.Open();
 
@@ -39,12 +35,12 @@ namespace Sepa.Controllers
                 SqlCommand cmd2 = new SqlCommand("PopulateInvoices", conn);
                 cmd2.CommandType = CommandType.StoredProcedure;
                 cmd2.ExecuteNonQuery();
-                //dBEntities.Database.ExecuteSqlCommand("insert into Vendors values(998,'vendor tes2','Add6', 'New York', 'USA', 'USD/PL', 'v3@test.com', '0015552316777');");
-                //                var query = dBEntities.Vendors.SqlQuery("select * from Vendors").ToList<Table1>();
-                //                var query2 = dBEntities.Database.SqlQuery<Table1>("select * from Table1").ToList<Table1>();
-                //               var query3 = dBEntities.Database.SqlQuery<tablevm>("select t1.id, t1.product, SUM(t1.price) as price from Table1 as t1 group by t1.id, t1.product").ToList<tablevm>();
 
-            }
+                SqlCommand cmd3 = new SqlCommand("AnonymiseData", conn);
+                cmd3.CommandType = CommandType.StoredProcedure;
+                cmd3.ExecuteNonQuery();
+
+             }
 
             return View();
 
