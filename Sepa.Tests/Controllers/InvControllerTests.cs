7cb66a8ba4ca3139272e7971fc69e6ae2931e209
Colm Carberry <ccarberry@live.com>
2019-06-04 11:53:06 +0100

    TestInvController

---
+++ Sepa.Tests/Controllers/InvControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sepa.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sepa.Controllers.Tests
+{
+    [TestClass()]
+    public class InvControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SubmitSelectedTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
